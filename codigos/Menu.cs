5c709ca4d047fa2614ff45c33df3c12ebfca158e
bruno <brunomirandadias2014@gmail.com>
2026-02-03 20:57:14 -0300

    Add files via upload

---
+++ codigos/Menu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject Iniciar;
+    [SerializeField] GameObject users;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Creditos()
+    {
+       users.SetActive(true);
+    }
+    public void NovoJogo()
+    {
+        Iniciar.SetActive(true);
+    }
+    public void Volta()
+    {
+       users.SetActive(false);
+    }
+    public void Ok()
+    {
+        SceneManager.LoadScene("principal");
+    }
+}
---
+++ codigos/teste.cs
@@ -0,0 +1,136 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+
+
+public class teste : MonoBehaviour
+{
+    public bool correto;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        /* Renderer rend = GetComponent<Renderer>();
+         rend.material.color = Color.red;
+        */
+       // if(movimento_Player.GetComponent<movimento_Player>().correto == true)
+        
+        TesteDivisao();
+        TesteMulti();
+
+    }
+    [SerializeField] Text texto;
+    [SerializeField] InputField resposta;
+
+    bool ativo = false;
+    int HP = 20;
+    float tempo = 0;
+    // Update is called once per frame
+    KeyCode[] key = { KeyCode.Space, KeyCode.Keypad0, KeyCode.Keypad1, KeyCode.Keypad2, KeyCode.Keypad3, KeyCode.Keypad4, KeyCode.Keypad5, KeyCode.Keypad6, KeyCode.Keypad7, KeyCode.Keypad8, KeyCode.Keypad9, };
+    KeyCode[] User = new KeyCode[3];
+    //List<int> User = new List int[3];
+    int i;
+
+    string inprimir, resultado;
+    float a = 0, b = 0, c = 0;
+    public void Update()
+    {
+        for (i = 0; i < key.Length; i++)
+        {
+            key[i] = (KeyCode)i;
+
+
+        }
+        for (i = 0; i < User.Length; i++)
+        {
+            // Verifica quando 3 dígitos foram digitados
+            if (resposta.text.Length == 3)
+            {
+                Verifica();
+            }
+        }
+
+        tempo += Time.deltaTime;
+        if (tempo >= 2 && ativo == true)
+        {
+            ativo = false;
+            resposta.text = "";
+            TesteMulti();
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Verifica();
+        }
+    }
+
+    public void TesteMulti()
+    {
+
+
+
+        a = Random.Range(0, 11);
+        b = Random.Range(0, 11);
+        c = a * b;
+        inprimir = a + " X " + b;
+        texto.text = inprimir;
+
+    }
+    public void TesteDivisao()
+    {
+
+        a = Random.Range(0, 11);
+        b = Random.Range(0, 11);
+        c = a / b;
+        inprimir = a + " / " + b;
+        texto.text = inprimir;
+    }
+
+    public void Verifica()
+    {
+
+        int Usuario;
+
+        if (int.TryParse(resposta.text, out Usuario))
+        {
+            if (Usuario == c)
+            {
+
+                resultado = " Correto ";
+                texto.text = resultado;
+                Debug.Log("acerto");
+                Debug.Log("---------------------");
+                ativo = true;
+
+                correto = true;
+                tempo = 0;
+
+
+            }
+            else
+            {
+
+                correto = false;
+                resultado = " Errado!! ";
+                texto.text = resultado;
+
+                HP--;
+                Debug.Log("errro");
+                Debug.Log(HP);
+                Debug.Log("---------------------");
+
+                ativo = true;
+
+                tempo = 0;
+
+
+
+
+            }
+
+
+        }
+    }
+}
