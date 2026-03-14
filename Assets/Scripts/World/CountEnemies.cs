0732b52d1662c9efeb621d1de58533c1ed7cc86f
Christopher Alysson <chrisalysson29@gmail.com>
2026-03-14 03:19:29 -0300

    Add control to enemies spawn in islands

--- Assets/Scripts/World/CountEnemies.cs
+++ Assets/Scripts/World/CountEnemies.cs
@@ -1,16 +1,30 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CountEnemies : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] public List<GameObject> enemies = new List<GameObject>();
+    public List<GameObject> countEnemies = new List<GameObject>();
+
     void Start()
     {
-        
+        PopulateFromEnemies();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Preenche 'countEnemies' com instâncias baseadas na lista 'enemies'.
+    // Limpa a lista atual antes de popular para evitar duplicatas.
+    public void PopulateFromEnemies()
     {
-        
+        countEnemies.Clear();
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            var prefab = enemies[i];
+            if (prefab == null) continue;
+
+            var instance = Instantiate(prefab, transform.position, Quaternion.identity, transform);
+            instance.name = $"{prefab.name}_{i}";
+            countEnemies.Add(instance);
+        }
     }
 }
