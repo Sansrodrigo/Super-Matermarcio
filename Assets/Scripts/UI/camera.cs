3af3d6ef4fc95952c93a0997d7aa0bf289acf068
aluno <brunomirandadias2014@gmail.com>
2026-03-18 19:13:51 -0300

    atualizacao da camera

--- Assets/Scripts/UI/camera.cs
+++ Assets/Scripts/UI/camera.cs
@@ -7,7 +7,7 @@ public class camera : MonoBehaviour
     [SerializeField] GameObject camTarget; //Pode ser o player ou outra coisa
     void Update()
     {
-        if(SceneManager.GetActiveScene().name == "House0_F0")
+        if(SceneManager.GetActiveScene().name == "Gameplay" || SceneManager.GetActiveScene().name == "House0_F0")
         {
             transform.position = camTarget.transform.position + new Vector3(0f, 0f, -10f);
         }
