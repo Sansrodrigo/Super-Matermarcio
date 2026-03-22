ad4a3b648a985d4dff04d9fed413fdbe0687ef79
Daniel Foxyz <145469300+foxynhoz@users.noreply.github.com>
2026-03-22 06:09:56 -0300

    Fixed Audio Volume between Scenes (Excluding House)

--- Assets/Scripts/MANAGERS/AudioManager_Script.cs
+++ Assets/Scripts/MANAGERS/AudioManager_Script.cs
@@ -7,11 +7,12 @@ using UnityEngine.UI;
 
 public class AudioManager_Script : MonoBehaviour
 {
-
+    private static float musicVolume = 0.5f; //Variavel para armazenar o volume da musica
+    private static float SFXVolume = 0.5f; //Variavel para armazenar o volume da musica
 
     [Header("UI CONTROLLERS")]
-    [SerializeField] private Slider ImusicVolume; //Slider para controlar o volume da musica
-    [SerializeField] Slider ISFXVolume; //Slider para controlar o volume dos SFX
+    [SerializeField] Slider Music_Volume_Slider; //Slider para controlar o volume da musica
+    [SerializeField] Slider SFX_Volume_Slider; //Slider para controlar o volume dos SFX
 
     [Space(20)]
 
@@ -22,17 +23,19 @@ public class AudioManager_Script : MonoBehaviour
 
     void Start()
     {
+        Music_Volume_Slider.value = musicVolume; //Seta o valor do slider de musica para o valor armazenado
+        SFX_Volume_Slider.value = SFXVolume; //Seta o valor do slider de SFX para o valor armazenado
+
         AudioSources = GetComponents<AudioSource>(); //Pega os audiosources do objeto
         AudioSwitch(); //Chama a funcao que troca a musica dependendo da cena
     }
 
     void Update()
     {
-        AudioSources[0].volume = ImusicVolume.value; //Controla o volume da musica com o slider 
-        AudioSources[1].volume = ISFXVolume.value; //Controla o volume dos SFX com o slider
+        AudioSources[0].volume = musicVolume; //Controla o volume da musica com o slider 
+        AudioSources[1].volume = SFXVolume; //Controla o volume dos SFX com o slider
     }
 
-    [ContextMenu("Resetar Vida")]
     public void AudioSwitch() //Troca a musica dependendo da cena
     {
         switch (SceneManager.GetActiveScene().name)
@@ -50,5 +53,15 @@ public class AudioManager_Script : MonoBehaviour
                 AudioSources[0].Play();
                 break;
         }
+    }
+
+    //Volume Controls - Its called everytime the slider value changes
+    public void MusicVolumeControl() //Eh chamado toda vez que o slider de musica muda de valor
+    {
+        musicVolume = Music_Volume_Slider.value; //Armazena o valor do slider de musica na variavel
+    }
+    public void SFXVolumeControl() //Eh chamado toda vez que o slider de SFX muda de valor
+    {
+        SFXVolume = SFX_Volume_Slider.value; //Armazena o valor do slider de SFX na variavel
     }
 }
